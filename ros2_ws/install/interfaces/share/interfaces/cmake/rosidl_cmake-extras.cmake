1f406f41ddb1a7921bd817476b7bc4cf9da54b63
goldenrishabh <bkrishabhsharma@gmail.com>
2025-04-28 11:26:30 +0530

    recommit

--- ros2_ws/install/interfaces/share/interfaces/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(interfaces_IDL_FILES "msg/Actuator.idl;msg/DVL.idl")
-set(interfaces_INTERFACE_FILES "msg/Actuator.msg;msg/DVL.msg")
